ecd3838d0f4b7779e2f4775edb4f365e8b054dc6
bilitade <bilitade2022@gmail.com>
2024-11-10 17:37:35 +0300

    JWT authentication done

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -134,9 +134,10 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  
+        'rest_framework.authentication.SessionAuthentication', 
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('orders/<int:pk>', views.OrderDetailView.as_view()),
 
     path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+
+
     path('cart/menu-items', views.CartView.as_view()),
    path('groups/manager/users', views.ManagerUserView.as_view(), name='manager_user_list'),
     path('groups/manager/users/<int:userId>', views.ManagerUserDetailView.as_view(), name='manager_user_detail'),
